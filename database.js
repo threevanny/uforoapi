1c1fad28e59951111f881f89a0b9f25b2411716f
threevanny <54084164+threevanny@users.noreply.github.com>
2020-09-23 16:09:20 -0500

    conexion a la base de datos

---
+++ database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const monogDB_Local_URI = 'mongodb://localhost/uforo'
+const mongoDB_URI = process.env.MONGODB_URI || monogDB_Local_URI;
+
+mongoose.connect(mongoDB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => {
+  return console.log("Connection DB established");
+}).catch(err => {
+  return console.log(err);
+});
