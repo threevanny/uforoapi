546d245c35611eaad09916c269295b6fa6d90796
threevanny <54084164+threevanny@users.noreply.github.com>
2020-09-26 11:12:45 -0500

    update User Schema

--- models/User.js
+++ models/User.js
@@ -1,9 +1,20 @@
 const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
-  nombre: { type: String },
-  email: { type: String },
-  password: { type: String },
+  nombre: {
+    type: String,
+    required: [true, 'Nombre de usuario requerido.']
+  },
+  email: {
+    type: String, required: 'Email requerido',
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    index: { unique: true, dropDups: true }
+  },
+  password: {
+    type: String,
+    required: [true, 'Password requerida.']
+  },
   edad: { type: String },
   sexo: { type: String },
   permisos: { type: String }
