869401c11e13a9edc7bacd066ae50079ae919891
Geovanny <threevanny@localhost.localdomain>
2020-12-21 17:19:32 -0500

    use api routes in app.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index.routes');
-const apiRouter = require('./routes/api.routes');
-
+const userRouter = require('./routes/user.routes');
+const quetionRouter = require('./routes/question.routes');
+const replyRouter = require('./routes/reply.routes');
+const avatarRouter = require('./routes/avatar.routes');
 
 const app = express();
 
@@ -21,25 +23,28 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use(apiRouter);
+app.use(userRouter);
+app.use(quetionRouter);
+app.use(replyRouter);
+app.use(avatarRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 //database
 //require('./database');
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
